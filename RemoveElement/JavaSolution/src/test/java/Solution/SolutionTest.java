ed21838bf4fc2e235541892649a1c8ca5d8c49a8
Nikolas Begetis <nmpegetis@gmail.com>
2020-12-29 20:50:44 +0100

    Remove Element from array problem and solution

---
+++ RemoveElement/JavaSolution/src/test/java/Solution/SolutionTest.java
@@ -0,0 +1,40 @@
+package Solution;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class SolutionTest {
+
+    @Test
+    public void test() {
+        // given
+        int[] nums = new int[] { 3, 2, 2, 2, 3 };
+        int val = 2;
+        int[] expected_array = new int[] { 3, 3, 2, 2, 3 };
+        int expected_result = 2;
+
+        // when
+        int result = removeElement(nums, val);
+        // then
+        assertArrayEquals(expected_array, nums);
+        assertEquals(expected_result, result);
+    }
+
+    public int removeElement(int[] nums, int val) {
+        // count the times that val is found
+        int count = 0;
+        int write = 0;
+        int length = nums.length;
+        for (int itr = 0; itr < length; itr++) {
+            if (nums[itr] == val) {
+                count++;
+                continue;
+            }
+            nums[write] = nums[itr];
+            write++;
+        }
+        return nums.length - count;
+    }
+}
